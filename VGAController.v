f2a51b5d68f52d5af1b2fdbfc8f794668dc746ad
eltonkl <eltonkl@users.noreply.github.com>
2016-02-18 09:52:40 -0800

    Define input for VGAController

--- VGAController.v
+++ VGAController.v
@@ -1,11 +1,12 @@
 `include "Constants.v"
 
 module VGAController(
-    output reg [0:2]    Red,
-    output reg [0:2]    Green,
-    output reg [0:1]    Blue,
-    output              HSync,
-    output              VSync
+    output reg [0:2]                Red,
+    output reg [0:2]                Green,
+    output reg [0:1]                Blue,
+    output                          HSync,
+    output                          VSync,
+    input [`BITS_PER_BLOCK-1:0]     Blocks [0:`GRID_HEIGHT-1] [0:`GRID_WIDTH-1]
     );
 
 
