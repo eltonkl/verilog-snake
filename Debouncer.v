61a5db33c159635a102cd64164d204aa3a68abde
Anthony Lai <yukuan.anthony.lai@gmail.com>
2016-02-18 11:01:59 -0800

    Implement debouncer

--- Debouncer.v
+++ Debouncer.v
@@ -1,8 +1,24 @@
 module Debouncer(
     input wire 	Clock,
     input wire  Signal,
-    output wire Pressed
+    output wire Enabled
     );
+	
+	reg step;
+	reg stepPrev;
+	
+	assign Enabled = step && !step_prev;
+	
+	initial
+	begin
+		step = 0;
+		stepPrev = 0;
+	end
 
+	always @ (posedge Clock)
+	begin
+		step <= Signal;
+		stepPrev <= step;
+	end
 
 endmodule
