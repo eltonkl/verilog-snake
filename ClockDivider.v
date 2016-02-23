d2605191dd991bf9e76b1f6d407b7adf7a05835b
Anthony Lai <yukuan.anthony.lai@gmail.com>
2016-02-23 09:55:54 -0800

    Implement gameClock

--- ClockDivider.v
+++ ClockDivider.v
@@ -1,18 +1,32 @@
 module ClockDivider(
 	input		MasterClock,
 	output	reg	Clock
+	output  reg gameClock;
 	);
 	
-	reg clockCount;
+	parameter GameClockBreakPoint = 50000000;   // 2Hz
+	
+	reg clockCounter;
+	reg gameClockCounter;
 	
 	initial begin
 		Clock = 0;
-		clockCount = 0;
+		gameClock = 0;
+		clockCounter = 0;
+		gameClockCounter = 0;
 	end
 
 	always @ (posedge MasterClock) begin
-		clockCount <= ~clockCount;
-		Clock <= ~Clock && clockCount;
+		clockCounter <= ~clockCounter;
+		Clock <= ~Clock && clockCounter;
+		
+		if (gameClocker == GameClockBreakPoint) begin
+		    gameClockCounter <= 0;
+		    gameClock <= 1;
+		end else begin
+		    gameClockCounter <= gameClockCounter + 1;
+		    gameClock <= 0;
+		end
 	end
 
 endmodule
