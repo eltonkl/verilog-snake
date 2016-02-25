8d0453a2ae472f556b6a219d2000773ecbe5d8bc
Anthony Lai <yukuan.anthony.lai@gmail.com>
2016-02-25 11:12:07 -0800

    Add fast clock

--- ClockDivider.v
+++ ClockDivider.v
@@ -2,18 +2,24 @@ module ClockDivider(
 	input		MasterClock,
 	output	reg	Clock
 	output  reg gameClock;
+	output  reg fastClock;  // for seg display
 	);
 	
 	parameter GameClockBreakPoint = 50000000;   // 2Hz
+	parameter FastClockBreakPoint = 160000;
 	
 	reg clockCounter;
 	reg gameClockCounter;
+	reg fastClockCounter;
 	
 	initial begin
 		Clock = 0;
 		gameClock = 0;
+		fastClock = 0;
+		
 		clockCounter = 0;
 		gameClockCounter = 0;
+		fastClockCounter = 0;
 	end
 
 	always @ (posedge MasterClock) begin
@@ -23,14 +29,22 @@ module ClockDivider(
 		    Clock <= ~Clock;
 		end
 		
-		
-		if (gameClocker == GameClockBreakPoint) begin
+		if (gameClock == GameClockBreakPoint) begin
 		    gameClockCounter <= 0;
 		    gameClock <= 1;
 		end else begin
 		    gameClockCounter <= gameClockCounter + 1;
 		    gameClock <= 0;
 		end
+		
+		if (fastClock == FastClockBreakPoint) begin
+		    fastClockCounter <= 0;
+		    fastClock <= 1;
+		end else begin
+		    fastClockCounter <= fastClockCounter + 1;
+		    fastClock <= 0;
+		end
+		
 	end
 
 endmodule
